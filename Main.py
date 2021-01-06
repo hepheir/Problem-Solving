579ae248797f92b74ba1d78cb78531fb54a4308d
Hepheir <hepheir@gmail.com>
2021-01-07 06:04:38 +0900

    맞았습니다!!
    25000760
    Crammer 공식을 이용한 풀이

--- Main.py
+++ Main.py
@@ -1,21 +1,8 @@
-from fractions import Fraction as Frac
-
 a, b, c, d, e, f = map(int, input().split())
 
-if a == 0:
-    y = Frac(c, b)
-    x = Frac(f, d) - Frac(e, d) * y
-elif b == 0:
-    x = Frac(c, a)
-    y = Frac(f, e) - Frac(d, e) * x
-elif d == 0:
-    y = Frac(f, e)
-    x = Frac(c, a) - Frac(b, a) * y
-elif e == 0:
-    x = Frac(f, d)
-    y = Frac(c, b) - Frac(a, b) * x
-else:
-    x = (Frac(c, b) - Frac(f, e)) / (Frac(a, b) - Frac(d, e))
-    y = Frac(c, b) - Frac(a, b) * x
+# Crammer's raw
+det = (a*e - b*d)
+x = (c*e - b*f) // det
+y = (a*f - c*d) // det
 
 print(x, y)
