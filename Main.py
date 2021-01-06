217896b6d7f327953453de427441c51c7e76deb4
Hepheir <hepheir@gmail.com>
2021-01-07 05:48:23 +0900

    맞았습니다!!
    25000700

--- Main.py
+++ Main.py
@@ -1,8 +1,21 @@
 from fractions import Fraction as Frac
 
-a,b,c,d,e,f = map(int, input().split())
+a, b, c, d, e, f = map(int, input().split())
 
-x = (Frac(c, b) - Frac(f, e)) / (Frac(a, b) - Frac(d, e))
-y = Frac(c, b) - Frac(a, b) * x
+if a == 0:
+    y = Frac(c, b)
+    x = Frac(f, d) - Frac(e, d) * y
+elif b == 0:
+    x = Frac(c, a)
+    y = Frac(f, e) - Frac(d, e) * x
+elif d == 0:
+    y = Frac(f, e)
+    x = Frac(c, a) - Frac(b, a) * y
+elif e == 0:
+    x = Frac(f, d)
+    y = Frac(c, b) - Frac(a, b) * x
+else:
+    x = (Frac(c, b) - Frac(f, e)) / (Frac(a, b) - Frac(d, e))
+    y = Frac(c, b) - Frac(a, b) * x
 
 print(x, y)
