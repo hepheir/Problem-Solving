a7a658d8fca011d48af2636d26fa06d3d42517eb
Hepheir <hepheir@gmail.com>
2021-01-08 00:06:06 +0900

    시간 초과
    25032617

--- Main.py
+++ Main.py
@@ -2,81 +2,87 @@ import sys
 input = sys.stdin.readline
 
 
-class Lines:
-    def __init__(self, n_of_lines: int):
-        self.x0 = [None] * n_of_lines
-        self.y0 = [None] * n_of_lines
-        self.x1 = [None] * n_of_lines
-        self.y1 = [None] * n_of_lines
-        self.parent = [None] * n_of_lines
-        self.subtree_size = [1] * n_of_lines
-        self.pointer = 0
+class Line(list):
+    pass
 
-    def append(self, x0: int, y0: int, x1: int, y1: int) -> None:
-        new_line = self.pointer
-        self.x0[new_line] = x0
-        self.y0[new_line] = y0
-        self.x1[new_line] = x1
-        self.y1[new_line] = y1
-        for old_line in range(new_line):
-            new_ancestor = self.find_ancestor(new_line)
-            old_ancestor = self.find_ancestor(old_line)
-            if new_ancestor is old_ancestor:
-                continue
-            if self.meets(new_line, old_line):
-                self.parent[new_ancestor] = old_ancestor
-                self.subtree_size[old_ancestor] += self.subtree_size[new_ancestor]
-        self.pointer += 1
 
-    def ccw(self, x0: int, y0: int, x1: int, y1: int, x2: int, y2: int) -> None:
-        tmp = (x0*y1 + x1*y2 + x2*y0) - (y0*x1 + y1*x2 + y2*x0)
-        if tmp > 0:
-            return 1
-        if tmp < 0:
-            return -1
-        return 0
+class LineId(int):
+    pass
 
-    def meets(self, l1: int, l2: int) -> bool:
-        c1 = self.ccw(self.x0[l1], self.y0[l1],
-                      self.x1[l1], self.y1[l1],
-                      self.x0[l2], self.y0[l2]) \
-            * self.ccw(self.x0[l1], self.y0[l1],
-                       self.x1[l1], self.y1[l1],
-                       self.x1[l2], self.y1[l2])
-        c2 = self.ccw(self.x0[l2], self.y0[l2],
-                      self.x1[l2], self.y1[l2],
-                      self.x0[l1], self.y0[l1]) \
-            * self.ccw(self.x0[l2], self.y0[l2],
-                       self.x1[l2], self.y1[l2],
-                       self.x1[l1], self.y1[l1])
+
+def ccw(x0: int, y0: int, x1: int, y1: int, x2: int, y2: int) -> int:
+    return (x0*y1 + x1*y2 + x2*y0) - (y0*x1 + y1*x2 + y2*x0)
+
+
+class Main:
+    def __init__(self):
+        self.N = int(input())
+        self.lines = [list(map(int, input().split())) for n in range(self.N)]
+        self.parents = [n for n in range(self.N)]
+        self.sizes = [1] * self.N
+
+    def solve(self):
+        roots = 0
+        maxsize = 0
+        for i in range(self.N-1):
+            for j in range(i, self.N):
+                i_parent = self.find_ancestor(i)
+                j_parent = self.find_ancestor(j)
+                if i_parent == j_parent:
+                    continue
+                if self.does_meet(i, j):
+                    self.merge_groups(j, i)
+        for line in range(self.N):
+            if self.is_root(line):
+                roots += 1
+                maxsize = max(maxsize, self.sizes[line])
+        print(roots)
+        print(maxsize)
+
+    def find_ancestor(self, line: LineId) -> LineId:
+        if self.parents[line] != line:
+            return self.find_ancestor(self.parents[line])
+        return line
+
+    def is_root(self, line: LineId) -> bool:
+        return line == self.find_ancestor(line)
+
+    def does_meet(self, line1: int, line2: int) -> bool:
+        c1 = ccw(
+            *self.lines[line1],
+            *self.lines[line2][:2]
+        ) * ccw(
+            *self.lines[line1],
+            *self.lines[line2][2:]
+        )
+        c2 = ccw(
+            *self.lines[line2],
+            *self.lines[line1][:2]
+        ) * ccw(
+            *self.lines[line2],
+            *self.lines[line1][2:]
+        )
+        # 일직선 상에 놓인경우, 직선이 곂치는지 검사
         if c1 * c2 == 0:
-            if max([self.x0[l1], self.x1[l1]]) < min([self.x0[l2], self.x1[l2]]):
+            if max(self.lines[line1][::2]) < min(self.lines[line2][::2]):
                 return False
-            if max([self.x0[l2], self.x1[l2]]) < min([self.x0[l1], self.x1[l1]]):
+            if max(self.lines[line2][::2]) < min(self.lines[line1][::2]):
                 return False
-            if max([self.y0[l1], self.y1[l1]]) < min([self.y0[l2], self.y1[l2]]):
+            if max(self.lines[line1][1::2]) < min(self.lines[line2][1::2]):
                 return False
-            if max([self.y0[l2], self.y1[l2]]) < min([self.y0[l1], self.y1[l1]]):
+            if max(self.lines[line2][1::2]) < min(self.lines[line1][1::2]):
                 return False
             return True
-        return c1 <= 0 and c2 <= 0
+        # 그렇지 않은 경우, 교차하는지 검사
+        else:
+            return c1 <= 0 and c2 <= 0
 
-    def find_ancestor(self, line: int) -> int:
-        while self.parent[line] is not None:
-            line = self.parent[line]
-        return line
+    def merge_groups(self, src: LineId, dst: LineId):
+        src_parent = self.find_ancestor(src)
+        dst_parent = self.find_ancestor(dst)
+        self.parents[src_parent] = dst_parent
+        self.sizes[dst_parent] += self.sizes[src_parent]
 
 
 if __name__ == "__main__":
-    N = int(input())
-    LINES = Lines(N)
-    for n in range(N):
-        LINES.append(*map(int, input().split()))
-    roots = 0
-    max_size = 0
-    for line in range(LINES.pointer):
-        if LINES.parent[line] is None:
-            roots += 1
-            max_size = max(max_size, LINES.subtree_size[line])
-    print(roots)
-    print(max_size)
+    Main().solve()
