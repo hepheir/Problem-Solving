834df943b76f89ccff813cf4d35255574960fc21
Hepheir <hepheir@gmail.com>
2021-01-22 15:07:34 +0900

    틀렸습니다

---
+++ 4949/25547963.py
@@ -0,0 +1,23 @@
+from sys import stdin
+
+stack = []
+isBalanced = True
+
+for line in stdin.read().split('.'):
+    if line and line != '\n':
+        stack = []
+        isBalanced = True
+        for c in line:
+            if c == '(':
+                stack.append(c)
+            elif c == ')':
+                if (not stack) or (stack.pop() != '('):
+                    isBalanced = False
+                    break
+            elif c == '[':
+                stack.append(c)
+            elif c == ']':
+                if (not stack) or (stack.pop() != '['):
+                    isBalanced = False
+                    break
+        print('yes' if isBalanced else 'no')
