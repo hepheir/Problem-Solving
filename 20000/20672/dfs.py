2b9d673eeb297d9cae0fa083ecb983861d8f514f
Hepheir <hepheir@gmail.com>
2021-01-28 19:03:49 +0900

    othercase 처리 방식 변경; (1+othercases) -> 예제 채점: 1o, 2x

--- 20000/20672/dfs.py
+++ 20000/20672/dfs.py
@@ -41,14 +41,9 @@ def predict(node, parent, state):
         return retval
     raise ValueError()
 
-def solve(node, parent=None, state=None, multiplier=None, cheatkey=None, debug=None):
-    # root node
-    if parent is None:
-        for child in graph[node]:
-            if child != parent:
-                solve(child, node, 0, 1, g[node], 'root')
+def solve(node, parent=None, state=-1, othercases=0, cheatkey=1, debug=''):
     # leaf node
-    elif len(graph[node]) == 1:
+    if (parent is not None) and (len(graph[node]) == 1):
         if node not in answer:
             answer[node] = 0
         if state == 2:
@@ -56,44 +51,51 @@ def solve(node, parent=None, state=None, multiplier=None, cheatkey=None, debug=N
             debug = log(debug,state,node,'x')
         else:
             # ends with (o)
-            debug = log(debug,state,node,'o')
             cheatkey = math.gcd(cheatkey, g[node])
-        answer[node] = modded_op(0, answer[node], multiplier * cheatkey, MOD)
-        print(f'{debug}\n\tcheatkey: {cheatkey}, mult: {multiplier}\n')
+            debug = log(debug,state,node,'o')
+        print(f'{debug}\n\tcheatkey: {cheatkey}, alts: {othercases}\n')
+        answer[node] += cheatkey * (1+othercases)
+        answer[node] %= MOD
     # otherwise
     else:
         predicts = dict()
         for exclude in graph[node]:
             if exclude == parent:
                 continue
-            for nextstate in (0, 1):
-                predicts[exclude, nextstate] = 1
-                for child in graph[node]:
-                    if child == parent or child == exclude:
-                        continue
-                    predicts[exclude,nextstate] = modded_op(nextstate, predicts[exclude,nextstate], predict(child, node, nextstate), MOD)
+            predicts[exclude, 0] = 0
+            predicts[exclude, 1] = 1
+            for child in graph[node]:
+                if child == parent or child == exclude:
+                    continue
+                predicts[exclude,0] = predicts[exclude,0] + predict(child,node,0)
+                predicts[exclude,1] = (predicts[exclude,1] % MOD) * (predict(child,node,1) % MOD) % MOD
 
         for child in graph[node]:
             if child != parent:
+                if state == -1:
+                    # (o) -1 -> 0
+                    solve(child, node, 0, 0, g[node], 'root')
+                    # (o) -1 -> 2
+                    solve(child, node, 2, 0, g[node], 'root')
                 if state == 0:
                     # (x) 0 -> 0, 2
-                    solve(child, node, 0, multiplier, cheatkey, log(debug,state,node,'x'))
+                    solve(child, node, 0, othercases, cheatkey, log(debug,state,node,'x'))
                     # (x) 0 -> 2, 0
-                    solve(child, node, 2, modded_op(state, multiplier, predicts[child,0], MOD), cheatkey, log(debug,state,node,'x'))
+                    solve(child, node, 2, othercases+predicts[child,0], cheatkey, log(debug,state,node,'x'))
                     # (o) 0 -> 1, 1
-                    solve(child, node, 1, modded_op(state, multiplier, predicts[child,1], MOD), math.gcd(cheatkey, g[node]), log(debug,state,node,'o'))
+                    solve(child, node, 1, othercases+predicts[child,1], math.gcd(cheatkey, g[node]), log(debug,state,node,'o'))
                     # (o) 0 -> 2, 1
-                    solve(child, node, 2, modded_op(state, multiplier, predicts[child,1], MOD), math.gcd(cheatkey, g[node]), log(debug,state,node,'o'))
+                    solve(child, node, 2, othercases+predicts[child,1], math.gcd(cheatkey, g[node]), log(debug,state,node,'o'))
                     continue
                 if state == 1:
                     # (x) 1 -> 2, 2
-                    solve(child, node, 2, multiplier, cheatkey, log(debug,state,node,'x'))
+                    solve(child, node, 2, othercases, cheatkey, log(debug,state,node,'x'))
                     # (o) 1 -> 1, 1
-                    solve(child, node, 1, modded_op(state, multiplier, predicts[child,1], MOD), math.gcd(cheatkey, g[node]), log(debug,state,node,'o'))
+                    solve(child, node, 1, othercases+predicts[child,1], math.gcd(cheatkey, g[node]), log(debug,state,node,'o'))
                     continue
                 if state == 2:
                     # (x) 2 -> 2, 2
-                    solve(child, node, 2, multiplier, cheatkey, log(debug,state,node,'x'))
+                    solve(child, node, 2, othercases, cheatkey, log(debug,state,node,'x'))
                     continue
 
 
@@ -110,4 +112,5 @@ for n in range(N-1):
 answer = dict()
 solve(1)
 
+print(answer)
 print(*[answer[leaf] for leaf in sorted(answer)])
