862ff66a98da561506da0d2de737252217ecbbe3
Hepheir <hepheir@gmail.com>
2021-01-31 02:03:21 +0900

    class Tree 를 이용한 좀 가지런한 코드 (2번예제는 fail)

--- 20000/20672/.py
+++ 20000/20672/.py
@@ -1,166 +1,119 @@
 import sys
 import math
 import collections
+import functools
+
+sys.setrecursionlimit(10**6)
 
 # WARNING THIS IS FOR DEBUGGING
 sys.stdin = open(f'20000/20672/data/boj.sample.{2}.in', 'r')
 
-def children(node, parent):
-    for child in graph[node]:
-        if child != parent:
-            yield child
-
-
-def siblings(node, parent, child):
-    for sibling in children(node, parent):
-        if sibling != child:
-            yield sibling
-
-
-def estimate(node, parent, state):
-    if state == -1:
-        case_o = 1
-        for child in children(node, parent):
-            case_o *= estimate(child, node, 0) + 1
-            # 각 자식은 0혹은 2의 상태를 가질 수 있으므로
-            # state(0) or state(2)
-            # => state(0) + state(2)
-        return case_o
-    if state == 0:
-        case_o = 1
-        case_x = 0
-        for child in children(node, parent):
-            case_o *= estimate(child, node, 1) + 1
-            # 각 자식은 1혹은 2의 상태를 가질 수 있으므로
-            # state(1) or state(2)
-            # => state(1) + state(2)
-            case_x += estimate(child, node, 0)
-            # 한 번에 하나의 자식만 0상태, 나머지는 2이므로
-            # state(0) and state(2) and state(2) and ...
-            # => state(0) * state(2) * state(2) * ...
-        return case_o + case_x
-    if state == 1:
-        case_o = 1
-        for child in children(node, parent):
-            case_o *= estimate(child, node, 1) + 1
-            # 각 자식은 1혹은 2의 상태를 가질 수 있으므로
-            # state(1) or state(2)
-            # => state(1) + state(2)
-        return case_o
-    if state == 2:
-        return 1
-
-
-def update_cheatkey(node, cheatkey):
-    return math.gcd(cheatkey, g[node])
-
-
-def case_x_multiverses(node, parent, child, multiverses):
-    # only one of the sibling is 0, the others are 2
-    retval = 0
-    for sibling in siblings(node, parent, child):
-        retval += estimate(sibling, node, 0)
-    return retval * multiverses
-
-
-def case_o_multiverses(node, parent, child, multiverses):
-    # all siblings can be both 1 or 2
-    retval = 1
-    for sibling in siblings(node, parent, child):
-        retval *= estimate(sibling, node, 1) + estimate(sibling, node, 2)
-    return retval * multiverses
-
-
-def is_leaf(node, parent):
-    # return not len(children(node, parent)) # TypeError: object of type 'generator' has no len()
-    return (parent) and (len(graph[node]) == 1)
-
-
-def update_log(node, state, log=tuple()):
-    genom = None
-    if node is not None:
-        genom = g[node]
-    return log+((genom, state),)
-
-
-def solve(node, parent, state, cheatkey, multiverses, log=tuple()):
-    if is_leaf(node, parent):
-        if  state != 2:
-            # (o)
-            cheatkey = update_cheatkey(node, cheatkey)
-            log = update_log(node, state, log)
-        answer[node] += cheatkey * multiverses
-        statistic.append((node, cheatkey, multiverses, log))
-        return
-    if state == -1:
-        # (o)
-        cheatkey = g[node]
-        multiverses = 1
-        for child in children(node, parent):
-            solve(child, node, 0, cheatkey, multiverses, update_log(node, state, log))
-            solve(child, node, 2, cheatkey, multiverses, update_log(node, state, log))
-        return
-    if state == 0:
-        # (x)
-        for child in children(node, parent):
-            solve(child, node, 0, cheatkey, multiverses, update_log(0, state, log)) # all the others should be 2 state
-            solve(child, node, 2, cheatkey, multiverses, update_log(0, state, log)) # all the others are 2
-            solve(child, node, 2, cheatkey, case_x_multiverses(node, parent, child, multiverses), update_log(0, state, log)) # one of the others can be 0 state
-        # (o)
-        cheatkey = update_cheatkey(node, cheatkey)
-        for child in children(node, parent):
-            solve(child, node, 1, cheatkey, case_o_multiverses(node, parent, child, multiverses), update_log(node, state, log))
-            solve(child, node, 2, cheatkey, case_o_multiverses(node, parent, child, multiverses), update_log(node, state, log))
-        return
-    if state == 1:
-        # (x)
-        for child in children(node, parent):
-            solve(child, node, 2, cheatkey, multiverses, update_log(0, state, log))
-        # (o)
-        cheatkey = update_cheatkey(node, cheatkey)
-        for child in children(node, parent):
-            solve(child, node, 1, cheatkey, case_o_multiverses(node, parent, child, multiverses), update_log(node, state, log))
-        return
-    if state == 2:
-        # (x)
-        for child in children(node, parent):
-            solve(child, node, 2, cheatkey, multiverses, update_log(0, state, log))
-        return
-
-
 MOD = 10**9+7
 
-N = int(sys.stdin.readline())
-g = [0, *map(int, sys.stdin.readline().split())]
+CHECKED = True
+NOT_CHECKED = False
 
-graph = [list() for _ in range(N+1)]
+CLOSED = True
+NOT_CLOSED = False
+class Tree:
+    def __init__(self):
+        N = int(sys.stdin.readline())
+        self.g = [0, *map(int, sys.stdin.readline().split())]
+        self.graph = [list() for _ in range(N+1)]
+        self.parent = [n for n in range(N+1)]
+        self.answer = [0] * (N+1)
+        for n in range(N-1):
+            u, v = map(int, sys.stdin.readline().split())
+            self.graph[u].append(v)
+            self.graph[v].append(u)
+        self.make_subtree(1)
+        self.bfs(1)
 
-for n in range(N-1):
-    u, v = map(int, sys.stdin.readline().split())
-    graph[u].append(v)
-    graph[v].append(u)
+    def make_subtree(self, node):
+        for child in self.get_children(node):
+            self.set_parent(child, node)
+            self.make_subtree(child)
+
+    def is_root(self, node):
+        return node == self.get_parent(node)
+    
+    def is_leaf(self, node):
+        return (self.is_root(node) == False) and (len(self.graph[node]) == 1)
+    
+    def get_parent(self, node):
+        return self.parent[node]
+    
+    def set_parent(self, node, parent):
+        self.parent[node] = parent
+
+    def get_children(self, node):
+        for child in self.graph[node]:
+            if child != self.get_parent(node):
+                yield child
+
+    def get_siblings(self, node):
+        for sibling in self.get_children(self.get_parent(node)):
+            if sibling != node:
+                yield sibling
+
+    @functools.cache
+    def estimate_othercases(self, node, is_checked, is_closed):
+        if is_checked:
+            retval = 1
+            for child in self.get_children(node):
+                retval *= self.estimate_othercases(child, CHECKED, NOT_CLOSED) \
+                        + self.estimate_othercases(child, NOT_CHECKED, CLOSED)
+            return retval
+        elif is_closed:
+            return 1
+        else:
+            retval = 0
+            for child in self.get_children(node):
+                retval += self.estimate_othercases(child, NOT_CHECKED, NOT_CLOSED)
+            return retval
+    
+    @functools.cache
+    def othercases(self, child, is_parent_checked):
+        if is_parent_checked:
+            retval = 1
+            for sibling in self.get_siblings(child):
+                retval *= self.estimate_othercases(sibling, CHECKED, NOT_CLOSED) \
+                        + self.estimate_othercases(sibling, NOT_CHECKED, CLOSED)
+            return retval
+        else:
+            retval = 0
+            for sibling in self.get_siblings(child):
+                retval += self.estimate_othercases(sibling, CHECKED, NOT_CLOSED)
+            return retval
+    
+    def bfs(self, root):
+        first_child = next(self.get_children(root))
+        deque = collections.deque()
+        deque.append((first_child, CHECKED, NOT_CLOSED, self.g[root]))
+        deque.append((first_child, NOT_CHECKED, NOT_CLOSED, self.g[root]))
+        while deque:
+            node, is_checked, is_closed, cheatkey = deque.popleft()
+
+            if is_checked == CHECKED: # (?) -> ... -> (o) -> opened
+                cheatkey = math.gcd(self.g[node], cheatkey)
+                for child in self.get_children(node):
+                    for _ in range(self.othercases(child, is_parent_checked=CHECKED)):
+                        deque.append((child, CHECKED, NOT_CLOSED, cheatkey))
+                        deque.append((child, NOT_CHECKED, CLOSED, cheatkey))
+
+            elif is_closed == CLOSED: # (o) -> ... -> (x) -> closed
+                for child in self.get_children(node):
+                    deque.append((child, NOT_CHECKED, CLOSED, cheatkey))
+
+            else: # (x) -> ... -> (x) -> opened
+                for child in self.get_children(node):
+                    for _ in range(self.othercases(child, is_parent_checked=NOT_CHECKED)):
+                        deque.append((child, NOT_CHECKED, CLOSED, cheatkey))
+                    deque.append((child, CHECKED, NOT_CLOSED, cheatkey))
+                    deque.append((child, NOT_CHECKED, NOT_CLOSED, cheatkey))
+
+            if self.is_leaf(node):
+                self.answer[node] += cheatkey
 
 
-statistic = []
-answer = collections.defaultdict(lambda: 0)
-
-solve(1, 0, -1, 0, 0)
-# dfs start node: 1 (root node)
-# parent: 0 (no parent)
-# state: -1 (initial value, indicates root)
-# cheatkey: 0 (initial value)
-# multiverses: 0 (initial value)
-
-statistic.sort()
-for node, cheatkey, multiverses, log in statistic:
-    print(f'e:{node:<2d} c:{cheatkey:<10d} m:{multiverses:<4d} ', end='')
-    for g, state in log:
-        if not g:
-            g = 'x'
-        print(f' -[{state}]-> {g}', end='')
-    print()
-        
-
-print(*(answer[node]%MOD for node in sorted(answer)))
-
-pass
+print(*(ans % MOD for ans in Tree().answer if ans))
