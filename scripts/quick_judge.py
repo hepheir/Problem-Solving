8d1c0e4b5c85bc754b6f8b2e83fc10ed234ea87f
Hepheir <hepheir@gmail.com>
2021-02-01 00:22:58 +0900

    임시 stderr를 이용하지 않고 런타임에러 감지

--- scripts/quick_judge.py
+++ scripts/quick_judge.py
@@ -8,7 +8,6 @@ DEFAULT_DATA = 'data'
 DEFAULT_TIMEOUT = 10
 
 TMP_STDOUT = 'tmp.stdout'
-TMP_STDERR = 'tmp.stderr'
 
 pid = int(sys.argv[1] if len(sys.argv) == 2 else input('문제 번호: '))
 
@@ -17,34 +16,33 @@ source = os.path.join(problem_path, DEFAULT_SOURCE)
 data = os.path.join(problem_path, DEFAULT_DATA)
 
 for data_in, data_out in zip(*map(lambda ext: glob.glob(os.path.join(data,'*'+ext)), ['.in', '.out'])):
-    with open(TMP_STDOUT, 'w') as stdout, open(TMP_STDERR, 'w') as stderr:
+    with open(TMP_STDOUT, 'w') as stdout:
         verdict = ''
         try:
             subprocess.run(f'''python3 -c "import py_compile; py_compile.compile(r'{source}')"''')
             subprocess.run(f'''python3 "{source}"''',
                             stdin=open(data_in,'r'),
                             stdout=stdout,
-                            stderr=stderr,
+                            check=True,
                             timeout=DEFAULT_TIMEOUT)
-            with open(TMP_STDERR, 'r') as usr_stderr:
-                if usr_stderr.read():
-                    verdict = '런타임 에러'
+            with open(data_out, 'r') as ans_stdout, open(TMP_STDOUT, 'r') as usr_stdout:
+                ans = ans_stdout.read().rstrip()
+                usr = usr_stdout.read().rstrip()
+                if ans == usr:
+                    verdict = '맞았습니다!!'
+                elif not usr:
+                    verdict = '출력 없음'
                 else:
-                    with open(data_out, 'r') as ans_stdout, open(TMP_STDOUT, 'r') as usr_stdout:
-                        ans = ans_stdout.read().rstrip()
-                        usr = usr_stdout.read().rstrip()
-                        if ans == usr:
-                            verdict = '맞았습니다!!'
-                        elif not usr:
-                            verdict = '출력 없음'
-                        else:
-                            verdict = '틀렸습니다'
+                    verdict = '틀렸습니다'
         except subprocess.TimeoutExpired:
             verdict = '시간 초과'
+        except subprocess.CalledProcessError:
+            verdict = '런타임 에러'
+        except BaseException as err:
+            print(err)
         finally:
             data_in = data_in.replace('\\', '/')
             print(f'{verdict:10s}\t{data_in}')
 
 os.remove(TMP_STDOUT)
-os.remove(TMP_STDERR)
     
