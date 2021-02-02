e6de0ad1bd46974a4af05d51e04b5fe71ccdcad6
Hepheir <hepheir@gmail.com>
2021-02-03 02:27:09 +0900

    불필요해진 스크립트 제거

--- scripts/dummy_dirs.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for i in range(1000, 20000+1, 1000):
-    dirname = str(i)
-    if not os.path.exists(dirname):
-        os.mkdir(dirname)
-    with open(os.path.join(dirname, '.gitkeep'), 'w'):
-        pass
