38cc7427b2f366753bb3d53bbc76c43861957b8a
Hepheir <hepheir@gmail.com>
2021-02-28 23:52:37 +0900

    옵션 설정 가능하도록 변경 & 코드 구조 개편

--- scripts/브라우져에서 사용할 코드/채점 현황 페이지에서 제출번호 클릭으로 클립보드에 파일명 복사.js
+++ scripts/브라우져에서 사용할 코드/채점 현황 페이지에서 제출번호 클릭으로 클립보드에 파일명 복사.js
@@ -1,29 +1,76 @@
 javascript: (() => {
-    const solution = document.getElementById('status-table').getElementsByTagName('tr');
-    for (let i=1; i<=20; i++) {
-        const cells = solution[i].getElementsByTagName('td');
-        cells[0].addEventListener('click', evt => {
-            const id = cells[0].innerText;
-            const verdict = cells[3].innerText;
-            const memory = cells[4].innerText;
-            const time = cells[5].innerText;
-            const lang = cells[6].innerText;
+    const option = {
+        includesMemoryOnCorrect: true,
+        includesTimeOnCorrect: true,
+        
+        includesExt: false,
 
-            var result = `${id} ${verdict}`;
-            if (verdict  == '맞았습니다!!') {
-                result += ` ${memory} KB ${time} ms`
-            }
+        useClipboard: true,
+        useAlert: false,
 
-            if (lang.includes('Python') || lang.includes('PyPy3')) {
-                result += '.py';
-            } else if (lang.includes('Java 11')) {
-                result += '.java'
-            } else if (lang.includes('C++')) {
-                result += '.cpp'
-            }
-
-            navigator.clipboard.writeText(result);
-        });
+        showDatetimeUsingAlert: true,
     };
-    alert('제출 번호를 클릭해 보세요.');
+
+    function onClick(event) {
+        const statusTableRow = event.target.parentNode.parentNode;
+        const cells = statusTableRow.getElementsByTagName('td');
+        const result = {
+            id: cells[1].innerText,
+            user: cells[2].innerText,
+            problem: cells[3].innerText,
+            verdict : cells[4].innerText,
+            memory: cells[5].innerText,
+            time: cells[6].innerText,
+            language: cells[7].innerText,
+            length: cells[8].innerText,
+            datetime: cells[9].children[0].getAttribute('data-original-title'),
+        };
+        const data = [ result.id, result.verdict ];
+        var filename = '';
+        var ext = '';
+
+        if (option.includesMemoryOnCorrect) data.push(result.memory+' KB');
+        if (option.includesTimeOnCorrect) data.push(result.time+' ms');
+        if (option.includesExt) {
+            if (result.language.includes('Python') || result.language.includes('PyPy3')) {
+                ext = '.py';
+            } else if (result.language.includes('Java 11')) {
+                ext = '.java';
+            } else if (result.language.includes('C++')) {
+                ext = '.cpp';
+            }
+        }
+
+        filename = data.join(' ') + ext;
+
+        if (option.useClipboard) navigator.clipboard.writeText(filename);
+        if (option.useAlert) alert(filename);
+
+        if (option.showDatetimeUsingAlert) alert(result.datetime);
+    }
+
+    const solution = document.getElementById('status-table').getElementsByTagName('tr');
+    for (let i=0; i<solution.length; i++) {
+        const celltype = (i == 0) ? 'th' : 'td';
+        const cell = document.createElement(celltype);
+
+        if (i == 0) {
+            cell.innerHTML = '파일명 생성';
+        } else {
+            const button = document.createElement('button');
+
+            button.style.borderRadius = '3px';
+            button.style.color = '#fff';
+            button.style.backgroundColor = '#3071a9';
+            button.style.borderColor = '#285e8e';
+            button.style.padding = '5px 10px';
+            button.style.fontSize = '12px';
+            button.style.lineHeight = '1.5';
+            button.innerHTML = '복사';
+    
+            button.addEventListener('click', onClick);
+            cell.appendChild(button);
+        }
+        solution[i].insertBefore(cell, solution[i].getElementsByTagName(celltype)[0]);
+    };
 })();
