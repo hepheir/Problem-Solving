a481aa2dd5ebdbf16f9ada455e156d4540f20ebe
Hepheir <hepheir@gmail.com>
2021-03-07 02:47:33 +0900

    파이썬 내장 최소 힙 모듈을 이용하여 날먹

---
+++ problem/1927 최소 힙/27035335 맞았습니다!! 34700 KB 164 ms.py
@@ -0,0 +1,15 @@
+import sys
+import heapq # 최소 힙 모듈 사용
+
+N = int(sys.stdin.readline())
+heap = []
+
+for n in range(N):
+    x = int(sys.stdin.readline())
+    if x > 0:
+        heapq.heappush(heap, x)
+    elif x == 0:
+        if not heap:
+            print('0')
+        else:
+            print(heapq.heappop(heap))
