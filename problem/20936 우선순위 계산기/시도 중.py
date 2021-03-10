d559771494e85dd8ebb1acd95623d476b8019c39
Hepheir <hepheir@gmail.com>
2021-03-10 09:53:26 +0900

    연결리스트 힙 조합으로 비비는 중

--- problem/20936 우선순위 계산기/시도 중.py
+++ problem/20936 우선순위 계산기/시도 중.py
@@ -1,18 +1,103 @@
 from __future__ import annotations
 
 import dataclasses
-import heapq
 import operator
 import typing
 
 
-@dataclasses.dataclass()
+class heapq:
+    def __init__(self):
+        self._list:typing.List[OperatorNode] = []
+
+
+    def __getitem__(self, key) -> OperatorNode:
+        return self._list[key]
+    
+
+    def __setitem__(self, key:int, item:OperatorNode):
+        self._list[key] = item
+        item.heap_idx = key
+
+
+    @property
+    def isempty(self):
+        return not self._list
+
+
+    @property
+    def size(self):
+        return len(self._list)
+
+    
+    def heappos(self, item:OperatorNode):
+        return item.heap_idx
+
+
+    # 파이썬 heapq 모듈에서 가져옴
+    def heappush(self, item):
+        """Push item onto heap, maintaining the heap invariant."""
+        self._list.append(item)
+        self.siftdown(0, self.size-1)
+
+
+    # 파이썬 heapq 모듈에서 가져옴
+    def heappop(self):
+        """Pop the smallest item off the heap, maintaining the heap invariant."""
+        lastelt = self._list.pop()    # raises appropriate IndexError if heap is empty
+        if self:
+            returnitem = self[0]
+            self[0] = lastelt
+            self.siftup(0)
+            return returnitem
+        return lastelt
+
+
+    # 파이썬 heapq 모듈에서 가져옴
+    def siftdown(self, startpos, pos):
+        newitem = self[pos]
+        # Follow the path to the root, moving parents down until finding a place
+        # newitem fits.
+        while pos > startpos:
+            parentpos = (pos - 1) >> 1
+            parent = self[parentpos]
+            if newitem < parent:
+                self[pos] = parent
+                pos = parentpos
+                continue
+            break
+        self[pos] = newitem
+
+
+    # 파이썬 heapq 모듈에서 가져옴
+    def siftup(self, pos):
+        endpos = self.size
+        startpos = pos
+        newitem = self[pos]
+        # Bubble up the smaller child until hitting a leaf.
+        childpos = 2*pos + 1    # leftmost child position
+        while childpos < endpos:
+            # Set childpos to index of smaller child.
+            rightpos = childpos + 1
+            if rightpos < endpos and not self[childpos] < self[rightpos]:
+                childpos = rightpos
+            # Move the smaller child up.
+            self[pos] = self[childpos]
+            pos = childpos
+            childpos = 2*pos + 1
+        # The leaf at pos is empty now.  Put newitem there, and bubble it up
+        # to its final resting place (by sifting its parents down).
+        self[pos] = newitem
+        self.siftdown(startpos, pos)
+
+
+@dataclasses.dataclass(unsafe_hash=True)
 class OperatorNode:
     operator: typing.Callable[[int, int], int]
     priority: int
     order: int
     left:NumberNode = dataclasses.field(default=None)
     right:NumberNode = dataclasses.field(default=None)
+    heap_idx:int = dataclasses.field(default=None)
 
     @property
     def value(self):
@@ -80,20 +165,10 @@ class LinkedList:
             node.right.right.left = node.left
         self.n_operators -= 1
 
-    
-    def pop(self):
-        node:OperatorNode = self.front.right
-        return_node = node
-        while node is not None:
-            if node < return_node:
-                return_node = node
-            node = node.right.right
-        return return_node
 
-
-if __name__ == '__main__':
+def solve():
     linkedList = LinkedList()
-    heap:typing.List[OperatorNode] = []
+    heap = heapq()
 
     S = input()
     number = 0
@@ -119,12 +194,20 @@ if __name__ == '__main__':
     
     node:OperatorNode = linkedList.front.right
     while node is not None:
-        heapq.heappush(heap, node)
+        heap.heappush(node)
         node = node.right.right
-    if not heap:
+
+    if heap.isempty:
         print(linkedList.front.value)
-    else:
-        while len(heap) > 1:
-            linkedList.remove_operator(heap[0])
-            heapq.heappop(heap)
-        print(heap[0].value)
+        return
+
+    while heap.size > 1:
+        linkedList.remove_operator(heap[0])
+        node = heap.heappop()
+        if node.left.left is not None:
+            heap.siftup(heap.heappos(node.left.left))
+        if node.right.right is not None:
+            heap.siftup(heap.heappos(node.right.right))
+    print(heap[0].value)
+
+solve()
