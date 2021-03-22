5b270b54e0a98074009da87f306e87ecb733dd3b
김동주 <hepheir@gmail.com>
2021-03-22 15:13:09 +0900

    BFS로 풀이 시도

---
+++ problem/9019 DSLR/27550532 시간 초과.py
@@ -0,0 +1,32 @@
+import collections
+import sys
+
+
+def D(n):
+    return (n << 1) % 10000
+
+def S(n):
+    return n-1
+
+def L(n):
+    return (n * 10 + n // 1000) % 10000
+
+def R(n):
+    return (n % 10) * 1000 + n // 10
+
+
+for t in range(int(sys.stdin.readline())):
+    A, B = map(int, sys.stdin.readline().split())
+    visited = collections.defaultdict(lambda: False)
+    deque = collections.deque([(A, '')])
+    while deque:
+        n, log = deque.popleft()
+        if n == B:
+            print(log)
+            break
+        if not visited[n]:
+            visited[n] = True
+            deque.append((D(n), log+'D'))
+            deque.append((S(n), log+'S'))
+            deque.append((L(n), log+'L'))
+            deque.append((R(n), log+'R'))
