3565ce464f03b47070ac519508ddeda687f7b18e
Hepheir <hepheir@gmail.com>
2021-07-26 23:12:57 +0900

    맞았습니다!!
    -   채점번호: 31484282
    -   메모리(KB): 61768
    -   시간(ms): 868
    -   풀이:
    
    요점은 우선순위 큐에 있었다.
    
    우선순위 큐를 이용한 데이크스트라 알고리즘을 사용하면, 상당히 빠르게 모든 노드의 최단 경로를 찾을 수 있다.
    
    또한 새롭게 알게 된 것이 있는데, 파이썬에서는 `float('inf')`를 통해 무한대와 비슷한 성질을 흉내낼 수 있다는 것이다. 단, `float('inf')`를 통해 생성한 `inf` 값은 숫자 `0`과 `==`, `>=`, `<=`의 연산을 수행시 모두 참이 나와버리기 때문에, 비교연산시 유의해야 한다.
    
    참조: https://justkode.kr/algorithm/python-dijkstra

--- Baekjoon Online Judge/problem/1753 최단경로/BOJ_1753.py
+++ Baekjoon Online Judge/problem/1753 최단경로/BOJ_1753.py
@@ -1,48 +1,61 @@
-from __future__ import annotations
-
-from collections import deque
-from dataclasses import dataclass, field
+from collections import defaultdict
+from heapq import heappop, heappush
 from sys import stdin
-from typing import Deque, Dict
+from typing import DefaultDict, Dict, Union
 
 
-@dataclass
-class Node:
-    cost: int | None = field(default=None) # dp로 구할 0부터 이 노드까지의 최단 경로(누적 가중치)
-    edges: Dict[Node, int] = field(default_factory=lambda: dict()) # 간선 정보가 담긴 딕셔너리 {도착노드: 가중치}
+def dijkstra(graph: Dict[int, Dict[int, int]], root: int) -> DefaultDict[int, Union[int, float]]:
+    heap = []
+    distances = defaultdict(lambda: float('inf'))
+    distances[root] = 0
+    heappush(heap, (distances[root], root))
+
+    while heap:
+        dist, node = heappop(heap)
+
+        if dist > distances[node]:
+            continue
+
+        for child, weight in graph[node].items():
+            new_dist = dist + weight
+
+            if new_dist > distances[child]:
+                continue
+
+            distances[child] = new_dist
+            heappush(heap, (new_dist, child))
+
+    return distances
 
 
 def solution():
     V, E = map(int, stdin.readline().split())
     K = int(stdin.readline())
 
-    nodes = [Node() for _ in range(20000+1)]
+    graph = defaultdict(lambda: dict())
 
     for _ in range(E):
         u, v, w = map(int, stdin.readline().split())
 
-        # 동일한 노드로 향하는 간선이 여러 개 있다면, 최단경로만 남김
-        if v in nodes[u].edges:
-            nodes[u].edges[v] = min(w, nodes[u].edges[v])
-        else:
-            nodes[u].edges[v] = w
+        if v not in graph[u] or w < graph[u][v]:
+            graph[u][v] = w
 
-    nodes[K].cost = 0
-    queue: Deque[Node] = deque([K])
-    while queue:
-        u = queue.popleft()
-        for v in nodes[u].edges:
-            new_cost = nodes[u].cost + nodes[u].edges[v]
-            if (nodes[v].cost is None) or (new_cost < nodes[v].cost):
-                nodes[v].cost = new_cost
-                queue.append(v)
+    distances = dijkstra(graph, K)
 
     for u in range(1, V+1):
-        if nodes[u].cost is None:
-            print('INF')
-        else:
-            print(nodes[u].cost)
+        print(str(distances[u]).upper())
 
 
 if __name__ == '__main__':
+    # import io
+    # stdin = io.TextIOWrapper(io.BytesIO())
+    # stdin.write('5 6\n'
+    #             '1\n'
+    #             '5 1 1\n'
+    #             '1 2 2\n'
+    #             '1 3 3\n'
+    #             '2 3 4\n'
+    #             '2 4 5\n'
+    #             '3 4 6\n')
+    # stdin.seek(0)
     solution()
