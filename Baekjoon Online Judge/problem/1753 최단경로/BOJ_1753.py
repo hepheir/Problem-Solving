f12d4ac6b60a2f87e515e6808af6ab5204fd286d
Hepheir <hepheir@gmail.com>
2021-07-26 22:21:52 +0900

    시간 초과
    -   채점번호: 31481930
    -   메모리(KB): -
    -   시간(ms): -
    -   풀이:
    
    문제의 알고리즘 분류를 참고하여 '데이크스트라 알고리즘'을 검색하였고, 내가 사용한 알고리즘과 크게 다르지 않아보인다고 생각했다.
    
    단순 코드의 효율만을 높여 시간초과 문제를 극복해보기 위해, 클래스 사용 지양, 동적 할당을 최대한 줄이기 위해 미리 메모리 할당 등 여러 노력을 해보았다.

--- Baekjoon Online Judge/problem/1753 최단경로/BOJ_1753.py
+++ Baekjoon Online Judge/problem/1753 최단경로/BOJ_1753.py
@@ -3,51 +3,45 @@ from __future__ import annotations
 from collections import deque
 from dataclasses import dataclass, field
 from sys import stdin
-from typing import Deque, List
-
-
-@dataclass
-class Edge:
-    start: Node # 출발할 노드
-    end: Node # 도착할 노드
-    weight: int # 가중치
+from typing import Deque, Dict
 
 
 @dataclass
 class Node:
     cost: int | None = field(default=None) # dp로 구할 0부터 이 노드까지의 최단 경로(누적 가중치)
-    edges: List[Edge] = field(default_factory=lambda: list())
+    edges: Dict[Node, int] = field(default_factory=lambda: dict()) # 간선 정보가 담긴 딕셔너리 {도착노드: 가중치}
 
 
 def solution():
     V, E = map(int, stdin.readline().split())
     K = int(stdin.readline())
 
-    graph = [Node() for _ in range(V+1)]
-    root = graph[K]
+    nodes = [Node() for _ in range(20000+1)]
 
     for _ in range(E):
         u, v, w = map(int, stdin.readline().split())
-        edge = Edge(start=graph[u],
-                    end=graph[v],
-                    weight=w)
-        graph[u].edges.append(edge)
 
-    root.cost = 0
-    queue: Deque[Node] = deque([root])
+        # 동일한 노드로 향하는 간선이 여러 개 있다면, 최단경로만 남김
+        if v in nodes[u].edges:
+            nodes[u].edges[v] = min(w, nodes[u].edges[v])
+        else:
+            nodes[u].edges[v] = w
+
+    nodes[K].cost = 0
+    queue: Deque[Node] = deque([K])
     while queue:
-        node = queue.popleft()
-        for edge in node.edges:
-            new_cost = edge.start.cost + edge.weight
-            if (edge.end.cost is None) or (new_cost < edge.end.cost):
-                edge.end.cost = new_cost
-                queue.append(edge.end)
+        u = queue.popleft()
+        for v in nodes[u].edges:
+            new_cost = nodes[u].cost + nodes[u].edges[v]
+            if (nodes[v].cost is None) or (new_cost < nodes[v].cost):
+                nodes[v].cost = new_cost
+                queue.append(v)
 
-    for node in graph[1:]:
-        if node.cost is None:
+    for u in range(1, V+1):
+        if nodes[u].cost is None:
             print('INF')
         else:
-            print(node.cost)
+            print(nodes[u].cost)
 
 
 if __name__ == '__main__':
