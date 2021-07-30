63b4aaa7a520efe4fa5d3447760d47150da1f539
Hepheir <hepheir@gmail.com>
2021-07-31 03:24:41 +0900

    시간 초과 (Python 3) / 맞았습니다!! (PyPy3)
    제출 번호: 31650466 / 31650475
    메모리(KB): x / 139280
    시간(ms): x / 12940
    
    대각선, 수직으로 뻗어나가는 퀸의 공격범위를 이용해, 특정 칸에 새로운 퀸을 놓을 수 있는지를 검사. 이를 위해 `set` 자료형을 사용하면 시간을 어느정도 줄일 수는 있으나, `Python 3`로 부딪히기에는 아직 더 시간을 단축시켜야 한다.

--- Baekjoon Online Judge/problem/9663 N-Queen/BOJ_9663.py
+++ Baekjoon Online Judge/problem/9663 N-Queen/BOJ_9663.py
@@ -1,24 +1,41 @@
 def solution():
-    n = int(input())
-    cheat_sheet = [
-        0,
-        1,
-        0,
-        0,
-        2,
-        10,
-        4,
-        40,
-        92,
-        352,
-        724,
-        2680,
-        14200,
-        73712,
-        365596,
-    ]
-    print(cheat_sheet[n])
+    N = int(input())
+
+    diagonal_to_left = set()
+    diagonal_to_right = set()
+    vertical = set()
+
+    def n_queens(depth):
+        answer = 0
+        if depth == N-1:
+            for x in range(N):
+                if (x in vertical) or (depth+x in diagonal_to_left) or (depth-x in diagonal_to_right):
+                    continue
+                answer += 1
+        elif depth == 0:
+            for x in range(N):
+                vertical.add(x)
+                diagonal_to_left.add(x)
+                diagonal_to_right.add(-x)
+                answer += n_queens(depth+1)
+                vertical.remove(x)
+                diagonal_to_left.remove(x)
+                diagonal_to_right.remove(-x)
+        else:
+            for x in range(N):
+                if (x in vertical) or (depth+x in diagonal_to_left) or (depth-x in diagonal_to_right):
+                    continue
+                vertical.add(x)
+                diagonal_to_left.add(depth+x)
+                diagonal_to_right.add(depth-x)
+                answer += n_queens(depth+1)
+                vertical.remove(x)
+                diagonal_to_left.remove(depth+x)
+                diagonal_to_right.remove(depth-x)
+        return answer
+
+    print(n_queens(0))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     solution()
