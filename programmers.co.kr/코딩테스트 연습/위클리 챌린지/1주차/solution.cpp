cf96951ff4006da205349179377822f90008908f
Hepheir-ZenBook <hepheir@gmail.com>
2021-08-03 15:04:06 +0900

    정답입니다!

--- programmers.co.kr/코딩테스트 연습/위클리 챌린지/1주차/solution.cpp
+++ programmers.co.kr/코딩테스트 연습/위클리 챌린지/1주차/solution.cpp
@@ -2,11 +2,12 @@ long long solution(int price, int money, int count)
 {
     int i, j;
     long long answer = money;
-    for (i=0;i<count;i++) {
-        for (j=0;j<=i;j++) {
+    for (i = 0; i < count; i++)
+    {
+        for (j = 0; j <= i; j++)
+        {
             answer -= price;
         }
     }
-    if (answer < 0) return -answer;
-    else return 0;
+    return (answer < 0) ? -answer : 0;
 }
