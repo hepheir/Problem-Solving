0543a0a06bdec559b409e0a36b068abf5aaae72b
Hepheir-ZenBook <hepheir@gmail.com>
2021-08-03 14:53:39 +0900

    코드 압축

--- programmers.co.kr/코딩테스트 연습/위클리 챌린지/1주차/solution.py
+++ programmers.co.kr/코딩테스트 연습/위클리 챌린지/1주차/solution.py
@@ -1,6 +1,2 @@
 def solution(price, money, count):
-    coeff = 0
-    for c in range(1, count+1):
-        coeff += c
-
-    return max(0, coeff*price - money)
+    return max(0, sum(range(1, count+1))*price - money)
